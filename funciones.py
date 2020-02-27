b65f1196720608997aeee72daa7f358c36471a2e
FranciscoRodRam <franckrodriguez06@gmail.com>
2020-02-27 08:16:12 -0600

    Funcion para leer archivo

--- funciones.py
+++ funciones.py
@@ -1,162 +1,44 @@
 # -- ------------------------------------------------------------------------------------ -- #
 # -- Proyecto: Laboratorio 2 de Trading                                -- #
-# -- Codigo: RepasoPython.py - describir brevemente el codigo                             -- #
 # -- Repositorio: https://github.com/FranciscoRodRam/LAB_2_FRRR                                                     -- #
 # -- Autor: Francisco Rodriguez Ramirez                                                               -- #
+# -- Codigo: RepasoPython.py - describir brevemente el codigo                             -- #
 # -- ------------------------------------------------------------------------------------ -- #
 
 # import numpy as np                                      # funciones numericas
-import pandas as pd                                       # dataframes y utilidades
-from datetime import timedelta                            # diferencia entre datos tipo tiempo
-from oandapyV20 import API                                # conexion con broker OANDA
-import oandapyV20.endpoints.instruments as instruments    # informacion de precios historicos
 
 
-# -- --------------------------------------------------------- FUNCION: Descargar precios -- #
-# -- Descargar precios historicos con OANDA
+# -- ------------------------------------------------------------------------------------ -- #
 
-def f_precios_masivos(p0_fini, p1_ffin, p2_gran, p3_inst, p4_oatk, p5_ginc):
-    """
+import pandas as pd
+
+
+# -- -------------------------------------------------------------------- FUNCION: leer archivo -- #
+def f_leer_archivo(param_archivo):
+    """"
     Parameters
     ----------
-    p0_fini
-    p1_ffin
-    p2_gran
-    p3_inst
-    p4_oatk
-    p5_ginc
+    param_archivos : str : nombre de archivo a leer
 
     Returns
     -------
-    dc_precios
+    df_data : pd.DataFrame : con informacion contenida en archivo leido
 
     Debugging
     ---------
+    param_archivo =  'archivo_tradeview_1.xlsx'
 
     """
+    # Leer archivo de datos y guardarlo en Data Frame
+    pd.read_excel('archivos/' + param_archivo, sheet_name='statement_vmaa')
 
-    def f_datetime_range_fx(p0_start, p1_end, p2_inc, p3_delta):
-        """
+    # Convertir a minusculas el nombre de las columnas
+    df_data.columns = [list(df_data.columns)[i].lower() for i in range(0, len(df_data.columns))]
 
-        Parameters
-        ----------
-        p0_start
-        p1_end
-        p2_inc
-        p3_delta
+    # Asegurar que ciertas columnas son tipo numerico
+    numcols = ['s/l', 't/p', 'commission', 'openprice', 'closeprice', 'profit', 'size'. 'swap', 'taxes', 'order']
+    df_data[numcols] = df_data[numcols].apply(pd.to_numeric)
 
-        Returns
-        -------
-        ls_resultado
+    #
 
-        Debugging
-        ---------
-        """
-
-        ls_result = []
-        nxt = p0_start
-
-        while nxt <= p1_end:
-            ls_result.append(nxt)
-            if p3_delta == 'minutes':
-                nxt += timedelta(minutes=p2_inc)
-            elif p3_delta == 'hours':
-                nxt += timedelta(hours=p2_inc)
-            elif p3_delta == 'days':
-                nxt += timedelta(days=p2_inc)
-
-        return ls_result
-
-    # inicializar api de OANDA
-
-    api = API(access_token=p4_oatk)
-
-    gn = {'S30': 30, 'S10': 10, 'S5': 5, 'M1': 60, 'M5': 60 * 5, 'M15': 60 * 15,
-          'M30': 60 * 30, 'H1': 60 * 60, 'H4': 60 * 60 * 4, 'H8': 60 * 60 * 8,
-          'D': 60 * 60 * 24, 'W': 60 * 60 * 24 * 7, 'M': 60 * 60 * 24 * 7 * 4}
-
-    # -- para el caso donde con 1 peticion se cubran las 2 fechas
-    if int((p1_ffin - p0_fini).total_seconds() / gn[p2_gran]) < 4999:
-
-        # Fecha inicial y fecha final
-        f1 = p0_fini.strftime('%Y-%m-%dT%H:%M:%S')
-        f2 = p1_ffin.strftime('%Y-%m-%dT%H:%M:%S')
-
-        # Parametros pra la peticion de precios
-        params = {"granularity": p2_gran, "price": "M", "dailyAlignment": 16, "from": f1,
-                  "to": f2}
-
-        # Ejecutar la peticion de precios
-        a1_req1 = instruments.InstrumentsCandles(instrument=p3_inst, params=params)
-        a1_hist = api.request(a1_req1)
-
-        # Para debuging
-        # print(f1 + ' y ' + f2)
-        lista = list()
-
-        # Acomodar las llaves
-        for i in range(len(a1_hist['candles']) - 1):
-            lista.append({'TimeStamp': a1_hist['candles'][i]['time'],
-                          'Open': a1_hist['candles'][i]['mid']['o'],
-                          'High': a1_hist['candles'][i]['mid']['h'],
-                          'Low': a1_hist['candles'][i]['mid']['l'],
-                          'Close': a1_hist['candles'][i]['mid']['c']})
-
-        # Acomodar en un data frame
-        r_df_final = pd.DataFrame(lista)
-        r_df_final = r_df_final[['TimeStamp', 'Open', 'High', 'Low', 'Close']]
-        r_df_final['TimeStamp'] = pd.to_datetime(r_df_final['TimeStamp'])
-
-        return r_df_final
-
-    # -- para el caso donde se construyen fechas secuenciales
-    else:
-
-        # hacer series de fechas e iteraciones para pedir todos los precios
-        fechas = f_datetime_range_fx(p0_start=p0_fini, p1_end=p1_ffin, p2_inc=p5_ginc,
-                                     p3_delta='minutes')
-
-        # Lista para ir guardando los data frames
-        lista_df = list()
-
-        for n_fecha in range(0, len(fechas) - 1):
-
-            # Fecha inicial y fecha final
-            f1 = fechas[n_fecha].strftime('%Y-%m-%dT%H:%M:%S')
-            f2 = fechas[n_fecha + 1].strftime('%Y-%m-%dT%H:%M:%S')
-
-            # Parametros pra la peticion de precios
-            params = {"granularity": p2_gran, "price": "M", "dailyAlignment": 16, "from": f1,
-                      "to": f2}
-
-            # Ejecutar la peticion de precios
-            a1_req1 = instruments.InstrumentsCandles(instrument=p3_inst, params=params)
-            a1_hist = api.request(a1_req1)
-
-            # Para debuging
-            print(f1 + ' y ' + f2)
-            lista = list()
-
-            # Acomodar las llaves
-            for i in range(len(a1_hist['candles']) - 1):
-                lista.append({'TimeStamp': a1_hist['candles'][i]['time'],
-                              'Open': a1_hist['candles'][i]['mid']['o'],
-                              'High': a1_hist['candles'][i]['mid']['h'],
-                              'Low': a1_hist['candles'][i]['mid']['l'],
-                              'Close': a1_hist['candles'][i]['mid']['c']})
-
-            # Acomodar en un data frame
-            pd_hist = pd.DataFrame(lista)
-            pd_hist = pd_hist[['TimeStamp', 'Open', 'High', 'Low', 'Close']]
-            pd_hist['TimeStamp'] = pd.to_datetime(pd_hist['TimeStamp'])
-
-            # Ir guardando resultados en una lista
-            lista_df.append(pd_hist)
-
-        # Concatenar todas las listas
-        r_df_final = pd.concat([lista_df[i] for i in range(0, len(lista_df))])
-
-        # resetear index en dataframe resultante porque guarda los indices del dataframe pasado
-        r_df_final = r_df_final.reset_index(drop=True)
-
-        return r_df_final
+    return df_data
