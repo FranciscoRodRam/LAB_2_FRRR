d8da5e4703fab26249c4723a4707d777d74968d2
FranciscoRodRam <franckrodriguez06@gmail.com>
2020-02-27 07:16:36 -0600

    Laboratorio 2

---
+++ datos.py
@@ -0,0 +1,8 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Laboratorio 2 de Trading                             -- #
+# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
+# -- Rep: https://github.com/FranciscoRodRam/LAB_2_FRRR    -- #
+# -- Autor: Francisco Rodriguez Ramirez                                                                  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+OA_Ak = '4' + '42de0e1239c7984ca4f78ed85957c84-9dfc793ef822512bb28ba0384553710' + '9'
