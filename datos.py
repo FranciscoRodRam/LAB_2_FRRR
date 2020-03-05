bd575745d89627f8c8dd916c21f456c22743f3e2
FranciscoRodRam <franckrodriguez06@gmail.com>
2020-03-05 08:30:58 -0600

    05.03.2020

--- datos.py
+++ datos.py
@@ -1,8 +1,9 @@
 # -- ------------------------------------------------------------------------------------ -- #
-# -- Proyecto: Laboratorio 2 de Trading                             -- #
-# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
-# -- Rep: https://github.com/FranciscoRodRam/LAB_2_FRRR    -- #
-# -- Autor: Francisco Rodriguez Ramirez                                                                  -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: funciones.py - script con datos de uso en proyecto
+# -- mantiene: Francisco Rodriguez
+# -- repositorio:https://github.com/FranciscoRodRam/LAB_2_FRRR
 # -- ------------------------------------------------------------------------------------ -- #
+
 
 OA_Ak = '4' + '42de0e1239c7984ca4f78ed85957c84-9dfc793ef822512bb28ba0384553710' + '9'
