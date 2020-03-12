107e6ad7e12b3aade899dad75761b5ca798aff11
FranciscoRodRam <franckrodriguez06@gmail.com>
2020-03-12 08:37:12 -0600

    Laboratorio 2

--- funciones.py
+++ funciones.py
@@ -4,41 +4,44 @@
 # -- mantiene: Francisco Rodriguez procesamiento de datos
 # -- repositorio:https://github.com/FranciscoRodRam/LAB_2_FRRR
 # -- ------------------------------------------------------------------------------------ -- #
-
+import numpy as np
 import pandas as pd
 # -- --------------------------------------------------- FUNCION: Leer archivo de entrada -- #
 # -- ------------------------------------------------------------------------------------ -- #
 # -- Importar el archivo.
+# %% FUNCION: leer archivo
 
 def f_leer_archivo(param_archivo):
     """
-    Parameters
-    ----------
-    param_archivo : str : nombre de archivo a leer
-    Returns
-    -------
-    df_data : pd.DataFrame : con informacion contenida en archivo leido
+    Funcion para leer archivo en formato xlsx.
+    :param param_archivo: Cadena de texto con el nombre del archivo
+    :return: dataframe de datos importados de archivo excel
     Debugging
     ---------
+    param_archivo = 'archivo_tradeview_1.xlsx'
     """
-    param_archivo = 'archivo_tradeview1.xlsx'
 
     # Leer archivo
-    df_data = pd.read_excel('archivos/' + param_archivo, sheetname = 'Sheet1')
-    # Elegir solo renglones en los que la columna type == 'buy' | type == 'sell'
-    # Convertir en minúsculas el nombre de las columnas por si se encuentran en mayúsculas o mixtas
-    df_data.columns = [list(df_data.columns)[i].lower() for i in list(df_data.columns)]
+    df_data = pd.read_excel('archivos/' + param_archivo, sheet_name='Sheet1')
+
+    # Elegir renglones type == buy | type == sell
+    df_data = df_data[df_data.type != 'balance']
+
+    # Resetear indice
+    df_data = df_data.reset_index()
+
+    # Convertir en minusculas los titulos de las columnas
+    df_data.columns = [list(df_data.columns)[i].lower()
+                       for i in range(0, len(df_data.columns))]
+
+    # Asegurar ciertas columnas de tipo numerico
     numcols = ['s/l', 't/p', 'commission', 'openprice', 'closeprice', 'profit', 'size', 'swap', 'taxes', 'order']
     df_data[numcols] = df_data[numcols].apply(pd.to_numeric)
 
     return df_data
-
 # -- ------------------------------------------------------ FUNCION: Pips por instrumento -- #
 # -- ------------------------------------------------------------------------------------ -- #
 # -- calcular el tamaño de los pips por instrumento
-
-
-
 def f_pip_size(param_ins):
     """
     Parameters
@@ -56,33 +59,34 @@ def f_pip_size(param_ins):
     inst = param_ins.lower()
     # lista de pips por instrumento
     pips_inst = {
-
-
-
-
-
-
-
     }
     return pips_inst[inst]
 
+# -- ------------------------------------------------------ FUNCION:  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# --
+
 def f_columnas_datos(param_data):
     """
-    :param param_data: dataframe conteniendo por lo menos las columnas 'closetime' y 'opentime'
-    :return: dataframe ingresado mas columna 'time' que es la diferencia entre close y open
-    Debugging
-    --------
-    param_data = datos
     """
     # Convertir las columnas de closetime y opentime con to_datetime
     param_data['closetime'] = pd.to_datetime(param_data['closetime'])
     param_data['opentime'] = pd.to_datetime(param_data['opentime'])
-
     # Tiempo transcurrido de una operación
-
     param_data['tiempo'] = [(param_data.loc[i, 'closetime'] - param_data.loc[i, 'opentime']).delta / 1e9
-
                             for i in range(0, len(param_data['closetime']))]
-
+    return param_data
+# -- ------------------------------------------------------ FUNCION:  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# --
+def f_columnas_pips(param_data):
+    param_data['pips'] = np.zeros(len(param_data['type']))
+    for i in range(0, len(param_data['type'])):
+        if param_data['type'][i] == 'buy':
+            param_data['pips'][i] = (param_data.closeprice[i] - param_data.openprice[i]) * f_pip_size(
+                param_ins=param_data['symbol'][i])
+        else:
+            param_data['pips'][i] = (param_data.openprice[i] - param_data.closeprice[i]) * f_pip_size(
+                param_ins=param_data['symbol'][i])
     return param_data
 
--- principal.py
+++ principal.py
@@ -1,10 +1,10 @@
 # -- ------------------------------------------------------------------------------------ -- #
-# -- Proyecto: Laboratorio 2 de Trading                                -- #
-# -- Repositorio: https://github.com/FranciscoRodRam/LAB_2_FRRR                                                     -- #
-# -- Autor: Francisco Rodriguez Ramirez                                                               -- #
-# -- Codigo: RepasoPython.py - describir brevemente el codigo                             -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: funciones.py - para visualizar loos datos.
+# -- mantiene: Francisco Rodriguez
+# -- repositorio:https://github.com/FranciscoRodRam/LAB_2_FRRR
 # -- ------------------------------------------------------------------------------------ -- #
 
 import funciones as fn
 
-datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
+df_data = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
